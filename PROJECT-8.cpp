fee82bedaa3a9a704685d0490bbc7d0ceb839f13
Diamondluxe <hehediamondluxe@gmail.com>
2026-06-25 20:42:43 +0500

    PROJECT 8 completed and pushed

--- PROJECT-8.cpp
+++ PROJECT-8.cpp
@@ -19,7 +19,7 @@ public:
         tax_rate = rate;
     }
     virtual double CalcWithHoldingTax(double propertycost)=0;
-    virtual void DetailInfo() = 0;
+    virtual void DetailInfo(double propertycost) = 0;
     virtual ~TaxPayer(){}
 };
 
@@ -28,7 +28,6 @@ class FilerTaxPayer: public TaxPayer  //derived class
 private:  
     string NTN;
     int nFilerYears;
-    double cost;
 
 public:
     FilerTaxPayer(string name, string type, double rate, string ntn, int year)
@@ -36,24 +35,22 @@ public:
     {
         NTN = ntn;
         nFilerYears = year;
-        cost = 0;
     }
 
     double CalcWithHoldingTax(double propertycost)override
     {
-        cost = propertycost;
-        return (tax_rate / 100) * cost;
+        return (tax_rate / 100) * propertycost;
     }
 
-    void DetailInfo()override
+    void DetailInfo(double propertycost)override
     {
        cout<<"\n========= Filer Tax Payer ========="<<endl;
        cout<<"Payer Name    : "<<PayerName<<endl;
        cout<<"Payer Type    : "<<PayerType<<endl;
        cout<<"National Tax Number   : "<<NTN<<endl;
-       cout<<"Property Cost : "<<fixed<<setprecision(3)<<cost<<endl;
+       cout<<"Property Cost : "<<fixed<<setprecision(3)<<propertycost<<endl;
        cout<<"Tax Rate      : "<<tax_rate<<"%"<<endl;
-       cout<<"Withholding Tax     : "<<CalcWithHoldingTax(cost)<<endl;
+       cout<<"Withholding Tax: "<<CalcWithHoldingTax(propertycost)<<endl;
        cout<<"Number of Years from which the Payer is filer: "<<nFilerYears<<endl;
        cout<<"********************************************"<<endl<<endl; 
     }
@@ -68,40 +65,37 @@ class NonFilerTaxPayer: public TaxPayer  //derived class
 {
 private:
     string IsEligible;
-    double cost;
 
 public:
     NonFilerTaxPayer(string name,string type,double rate)
     : TaxPayer(name,type, rate)
     {
         IsEligible = "";
-        cost = 0;
     }
 
     double CalcWithHoldingTax(double propertycost)override
     {
-        cost = propertycost;
-        if(cost<=3000000)
+        if(propertycost<=3000000)
         {
             IsEligible = "Yes";
-            return (tax_rate/100.0)*cost;
+            return (tax_rate/100.0)*propertycost;
         }
         else{
             IsEligible = "No";
            return 0;
         }
     }
-    void DetailInfo()override
+    void DetailInfo(double propertycost)override
     {
        cout<<"\n========= Non-Filer Tax Payer ========="<<endl;
        cout<<"Payer Name    : "<<PayerName<<endl;
        cout<<"Payer Type    : "<<PayerType<<endl;
        cout<<"Eligible      : "<<IsEligible<<endl;
-       cout<<"Property Cost : "<<fixed<<setprecision(2)<<cost<<endl;
+       cout<<"Property Cost : "<<fixed<<setprecision(2)<<propertycost<<endl;
        cout<<"Tax Rate      : "<<tax_rate<<"%"<<endl;
     
        if(IsEligible == "Yes")
-       cout<<"Withholding Tax: "<<CalcWithHoldingTax(cost)<<endl;
+       cout<<"Withholding Tax: "<<CalcWithHoldingTax(propertycost)<<endl;
        else
        cout<<"Not eligible to buy property above 30 lac."<<endl;
        
@@ -117,6 +111,8 @@ public:
 
 int main()
 {
+    cout<<"\nWELCOME BACK TO DIAMONDLUXE.. :)"<<endl<<endl;
+
     double PropertyCost;
 
     cout<<"Enter the cost of property to buy: ";
@@ -127,13 +123,13 @@ int main()
     TaxPayer *ptr;
     ptr = new FilerTaxPayer("Umar","Filer",2.500, "17301507", 3);
     ptr->CalcWithHoldingTax(PropertyCost);
-    ptr->DetailInfo();
+    ptr->DetailInfo(PropertyCost);
     delete ptr;
 
     ptr = new NonFilerTaxPayer("Ahmed","Non-Filer", 5.00);
     ptr->CalcWithHoldingTax(PropertyCost);
-    ptr->DetailInfo();
+    ptr->DetailInfo(PropertyCost);
     delete ptr;
 
-
+    cout<<"Thank YOU for using OUR FBR TAX MS ^-^"<<endl<<endl;
 }
